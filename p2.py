2fbd9cc493acaa8051195db76924f5f78cdf3c5b
JannaBanana <jyjung0113@gmail.com>
2016-11-28 11:04:49 +0900

    Add files via upload

--- p2.py
+++ p2.py
@@ -1,16 +1,45 @@
-import sys
-
 n = int(input("insert: "))
-count = 0
 for i in range (0, 2*n-1):
     if i < n:
-            for j in range (0, n):
-                if i+j<n-1:
-                    print ("_"," ", end="")
-                else:
-                    count = count + 1
-                    print(count, " ", end="")
-                sys.stdout.flush()
+        for j in range (0, n):
+            if i+j<n-1:
+                print ("_"," ", end="")
+            else:
+                print(2-n+i+j*j+j, " ", end="")
+        print()
+    else:
+        for j in range (0, n):
+            if i-j>=n:
+                print ("_"," ", end="")
+            else:
+                print(2 - n + i + ((j*j + j)), " ", end="")
+        print()
+
+
+import sys
+
+# n = int(input("insert: "))
+# count = 0
+# for i in range (0, 2*n-1):
+#     if i < n:
+#         for j in range (0, n):
+#             if i+j<n-1:
+#                 print ("_"," ", end="")
+#             else:
+#                 count = count + 1
+#                 print(count, " ", end="")
+#             sys.stdout.flush()
+#     else:
+#         for j in range (0, n):
+#             if i-j>=n:
+#                 print ("_"," ", end="")
+#             else:
+#                 count = count + 1
+#                 print (count, " ", end="")
+#             sys.stdout.flush()
+
+
+
     # else:
     #         for j in range (n, 2*n-1):
     #             if i+j>=2:
@@ -19,7 +48,7 @@ for i in range (0, 2*n-1):
     #                 count = count +1
     #                 print(count, " ", end="")
     #         sys.stdout.flush()
-    print ("\n")
+    # print ("\n")
 
 
 # n = 5
