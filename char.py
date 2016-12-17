5bb2c41ebad3cfd7307a5db7f7f19cbe374cc893
JannaBanana <jyjung0113@gmail.com>
2016-12-17 21:31:53 +0900

    Create char.py

---
+++ char.py
@@ -0,0 +1,12 @@
+
+a = "hello world"
+a_list = list(a)
+
+print(a, type(a))
+print(a_list, type(a_list))
+print(a_list[0], type(a_list[0]))
+#
+# for i in range(ord('0'), ord('9')+1):
+#     print(i, chr(i))
+#
+# print(v, ord(v), chr(97))
